8d12ba0475d75d45983a19b814262af5aacb049c
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-15 21:14:45 +0100

    the tow ways binding aspect

--- src/app/my-first-comp/my-first-comp.component.ts
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class MyFirstCompComponent {
 
-  name : string = "";
+  name : string = ""; //not private
   email : string = "";
   message : string = "";
 
