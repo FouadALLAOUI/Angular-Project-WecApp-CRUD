37c3b39da7ea826b73fa5733036c11ae0c71b865
FouadALLAOUI <allaoui70fouad71@gmail.com>
2024-04-17 16:46:01 +0100

    Create the service and fill it

--- src/app/services/my-first.service.ts
+++ src/app/services/my-first.service.ts
@@ -1,9 +1,46 @@
 import { Injectable } from '@angular/core';
 
-@Injectable({
+@Injectable({ 
   providedIn: 'root'
 })  // Dependency injection
 export class MyFirstService {
 
-  constructor() { }
+  messages: Array<any> = [];
+  
+  constructor() {
+    this.init();
+  }
+
+  init(): void{
+    this.insert({
+      name: "RIO",
+      email: "rio@gmail.com",
+      message:"lkjfmoireaoireaj"
+    });
+    this.insert({
+      name: "RIO",
+      email: "rio@gmail.com",
+      message:"lkjfmoireaoireaj"
+    });
+    this.insert({
+      name: "RIO",
+      email: "rio@gmail.com",
+      message:"lkjfmoireaoireaj"
+    });
+
+  }
+
+  insert(message: {name: string, email: string, message: string}){
+    this.messages.push(message);
+  }
+
+  getAllMessages(){
+    return this.messages;
+  }
+
+  deleteMessage(index: number){
+    this.messages.splice(index, 1);
+  }
+
+
 }
